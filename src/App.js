eacf418555fa00e43b0310d7346301e94304abd7
mikeprikhodko <mprykhodko@playq.net>
2019-03-29 16:52:35 +0200

    some moves with redux, store and actions

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import Navbar from "./components/Navbar"
 import UserBoard from "./components/UserBoard";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import AddUser from "./components/user/AddUser";
+import {Provider} from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-        <Router>
-            <div className="App">
-                <Navbar/>
-                <Route exact path="/" component={UserBoard}/>
-                <Route exact path="/addUser" component={AddUser}/>
-            </div>
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <div className="App">
+                    <Navbar/>
+                    <Route exact path="/" component={UserBoard}/>
+                    <Route exact path="/addUser" component={AddUser}/>
+                </div>
+            </Router>
+        </Provider>
     );
   }
 }
